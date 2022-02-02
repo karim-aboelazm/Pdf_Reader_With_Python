4125dd8088b50259c16a1c50e8c3f7b15ea50cf1
karim-aboelazm <karimaboelazm28@gmail.com>
2022-02-02 21:45:07 +0200

    pdf reader coding with python

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Pdf_Reader_With_Python" 
---
+++ main.py
@@ -0,0 +1,21 @@
+import PyPDF2 as pdf
+import pyttsx3 as pytts
+
+def text_to_voice(text):
+    engine = pytts.init('sapi5')
+    voice = engine.getProperty('voices')
+    engine.setProperty('voices',voice[1])
+    engine.setProperty('rate',150)
+    engine.say(text)
+    engine.runAndWait()
+
+def read_pdf_files(url,pn):
+    file = open(url, 'rb')
+    pdf_reader = pdf.PdfFileReader(file)
+    form_page = pdf_reader.getPage(pn-1)
+    txt = form_page.extractText()
+    text_to_voice(txt)
+
+read_pdf_files('E:\\Python\\Projects\\PdfReader\\new.pdf',6)
+
+
